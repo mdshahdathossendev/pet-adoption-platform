b5c204a38d0809b94b7037be39408f08188b57b9
hmdshahdat501-png <hmdshahdat501@gmail.comm>
2026-05-20 02:11:38 +0600

    losting end

--- src/app/(desbords)/desbord/page.jsx
+++ src/app/(desbords)/desbord/page.jsx
@@ -1,5 +1,6 @@
 
 import { auth } from '@/lib/auth';
+import { Table } from '@heroui/react';
 import { headers } from 'next/headers';
 import React from 'react';
 
@@ -10,12 +11,37 @@ const page = async() => {
         const user = session?.user;
         const id = user?.id
         const res = await fetch(`http://localhost:8000/listing/${id}`)
-        const data = await res.json();
-        console.log(data)
+        const datas = await res.json();
     return (
         <div className='mx-auto container ml-75'>
        <h2 className='text-2xl font-bold'>My Adoption <span className='text-orange-500'>Request</span></h2>
        <p className='mt-2'>Tak the status of all our adoption requests here</p>
+       <div className='w-4xl'>
+        <Table className='mt-6 mx-w-5xl'>
+      <Table.ScrollContainer>
+        <Table.Content aria-label="Team members" className="min-w-[600px]">
+          <Table.Header>
+            <Table.Column isRowHeader>Pet Name</Table.Column>
+            <Table.Column>Pickup Date</Table.Column>
+            <Table.Column>Status</Table.Column>
+            <Table.Column>Action</Table.Column>
+          </Table.Header>
+          <Table.Body>
+           {
+            datas.map(data => 
+                <Table.Row key={data._id}>
+              <Table.Cell>{data.name}</Table.Cell>
+              <Table.Cell>{data.pikUpdate}</Table.Cell>
+              <Table.Cell>{data.status}</Table.Cell>
+              <Table.Cell>michael@acme.com</Table.Cell>
+            </Table.Row>
+            )
+           }
+          </Table.Body>
+        </Table.Content>
+      </Table.ScrollContainer>
+    </Table>
+       </div>
         </div>
     );
 };
